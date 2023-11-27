54efebd33785c16774c68ba838e1edb6dc292608
vijayakumar1069 <vijay.r20799@gmail.com>
2023-11-28 00:24:07 +0530

    db connection established with mongodb

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,7 +1,21 @@
-const express=require("express")
-const app= express();
-const port=3000;
-app.listen(port,(req,res)=>
-{
-    console.log("port running",port)
-})
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const body_parser = require("body-parser");
+const dotenv = require("dotenv").config();
+const app = express();
+app.use(body_parser.json());
+app.use(express.json());
+app.use(cors());
+const mondbconnection = mongoose
+  .connect(process.env.URL)
+  .then(() => {
+    console.log("db connection established");
+  })
+  .catch((Error) => {
+    console.log("error connecting", Error);
+  });
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log("port running", process.env.PORT);
+});
