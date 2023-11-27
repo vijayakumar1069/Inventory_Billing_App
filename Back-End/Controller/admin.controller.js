f0eb196c582716bccdfc0c3cc4c03722ef5cb370
vijayakumar1069 <vijay.r20799@gmail.com>
2023-11-28 01:17:55 +0530

    admin login router done

--- Back-End/Controller/admin.controller.js
+++ Back-End/Controller/admin.controller.js
@@ -1,6 +1,7 @@
 const Admin = require("../Models/admin.model.js");
 const errorHandler = require("../Utils/errorHandler.js");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const signupRouter = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -20,10 +21,40 @@ const signupRouter = async (req, res, next) => {
     });
     await addnewadmin.save();
     const { password: pass, ...rest } = addnewadmin._doc;
-    res.status(200).json({rest });
+    res.status(200).json({ rest });
+  } catch (error) {
+    next(error);
+  }
+};
+const loginRouter = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(errorHandler(401, "email and password required"));
+    }
+    const findadminexists = await Admin.findOne({ email });
+
+    if (!findadminexists) {
+      return next(errorHandler(401, "Admin not found"));
+    }
+    const password_checking = bcrypt.compareSync(
+      password,
+      findadminexists.password
+    );
+    if (!password_checking) {
+      return next(errorHandler(401, "Wrong Email Or Password"));
+    }
+
+    const token = jwt.sign({ id: findadminexists._id }, process.env.JWT_KEY);
+    const { password: pass, ...rest } = findadminexists._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { signupRouter };
+module.exports = { signupRouter, loginRouter };
