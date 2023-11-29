70c47ead36cf537947766785a34613d6c7150956
vijayakumar1069 <vijay.r20799@gmail.com>
2023-11-29 06:01:31 +0530

    login UI and Functionality done added vertical line between img and form

--- Front-End/my-App/src/pages/Login.jsx
+++ Front-End/my-App/src/pages/Login.jsx
@@ -1,27 +1,85 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [formdata, setFormdata] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  console.log(formdata);
+  const handlechange = (e) => {
+    setFormdata({
+      ...formdata,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/admin/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formdata),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+      }
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
-    <div className='flex flex-col max-w-sm mt-20 md:mt-28 p-3 mx-auto md:max-w-lg lg:max-w-2xl '>
-        <div className="bg-gray-950 text-white p-3 text-center rounded-lg text-2xl">
-            <h1>Welcome to <span className='text-orange-600 font-semibold'>VIJAY's Billing App</span></h1>
+    <div className="flex flex-col max-w-sm mt-20 md:mt-28 p-3 mx-auto md:max-w-2xl lg:max-w-3xl rounded-lg ">
+      <div className="bg-gray-950 text-white p-3 text-center rounded-lg text-2xl">
+        <h1>
+          Welcome to{" "}
+          <span className="text-orange-600 font-semibold">
+            VIJAY's Billing App
+          </span>
+        </h1>
+      </div>
+      <div className="flex flex-col mt-10 p-5 gap-5 bg-purple-100 shadow-md md:shadow-lg lg:shadow-2xl rounded-t-xl md:flex-row ">
+        <div className="md:w-1/2  p-3">
+          <img
+            className="w-full h-full rounded-lg   "
+            src="https://images.unsplash.com/photo-1529539795054-3c162aab037a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bG9naW58ZW58MHx8MHx8fDA%3D"
+            alt="Login image"
+          />
         </div>
-        <div className="flex flex-col mt-10 p-5 gap-5 bg-purple-200 rounded-t-xl md:flex-row ">
-        <div className="md:w-1/2 md:border-r-2 md:border-pink-700 p-3">
-            <img className='w-full h-auto rounded-lg   '
-             src="https://images.unsplash.com/photo-1529539795054-3c162aab037a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bG9naW58ZW58MHx8MHx8fDA%3D" alt="Login image" />
+        <div className="md:border-r-2  md:border-pink-700"></div>
+        <div className="h-full">
+          <form className="flex flex-col gap-6  " onSubmit={handlesubmit}>
+            <input
+              type="email"
+              placeholder="email"
+              id="email"
+              className="boder p-3 rounded-lg "
+              onChange={handlechange}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              id="password"
+              className="boder p-3 rounded-lg "
+              onChange={handlechange}
+            />
+            <button className="bg-blue-800 p-3 rounded-lg uppercase text-white hover:opacity-80">
+              Login
+            </button>
+            <p>
+              if you don't have account{" "}
+              <Link to="/">
+                <span className="font-semibold text-red-800 ">SIGN UP </span>
+              </Link>{" "}
+            </p>
+          </form>
         </div>
-        <div className="">
-            <form className='flex flex-col gap-4 ' >
-                <input type="email" placeholder='email' id='email' className='boder p-3 rounded-lg ' />
-                <input type="password" placeholder='password' id='password' className='boder p-3 rounded-lg ' />
-                <button className='bg-blue-800 p-3 rounded-lg uppercase text-white hover:opacity-80'>Login</button>
-                <p>if you don't have account  <Link to="/" ><span className='font-semibold text-red-800 '>SIGN UP </span></Link> </p>
-            </form>
-        </div>
-        </div>
-        
+      </div>
     </div>
-  )
+  );
 }
--- Front-End/my-App/src/pages/Signup.jsx
+++ Front-End/my-App/src/pages/Signup.jsx
@@ -43,21 +43,23 @@ export default function Signup() {
     }
   };
   return (
-    <div className="flex  mt-20 p-10 max-w-sm  sm:max-w-md lg:max-w-2xl   flex-col mx-auto">
+    <div className="flex  mt-20 p-10 max-w-sm p-3  sm:max-w-2xl lg:max-w-4xl   flex-col mx-auto">
       <div className="bg-slate-200 p-3  ">
-        <h1 className="text-3xl text-center bg-slate-400 p-3 rounded-lg">
+        <h1 className="text-3xl text-center bg-violet-400 p-3 rounded-lg">
           Welcome to <span className="text-dc2626">VIJAY'S Billing APP</span>
         </h1>
       </div>
-      <div className="flex flex-col md:flex-row p-5 gap-5 items-center mt-10  bg-slate-800 rounded-lg">
-        <div className="w-full md:w-1/2    object-cover  md:border-slate-400  md:border-r-4">
+      <div className="flex flex-col md:flex-row p-5 gap-3  mt-10  bg-slate-800 rounded-lg ">
+        <div className="  md:w-full  object-cover items-center p-1 ">
           <img
-            src="https://images.unsplash.com/photo-1487611459768-bd414656ea10?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZvcm18ZW58MHx8MHx8fDA%3D"
+            src="https://images.unsplash.com/photo-1579444741963-5ae219cfe27c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9ybXxlbnwwfHwwfHx8MA%3D%3D"
             alt="Signup Image"
-            className="max-w-full h-auto  "
+            className="max-w-full h-full   "
           />
         </div>
-        <div className="p-1 w-full md:w-1/2 flex gap-1 flex-col">
+        <div className="md:border-r-2  md:border-pink-700"></div>
+
+        <div className="p-1 w-full md:w-full flex gap-1 flex-col">
           <form className="flex flex-col gap-2" onSubmit={handlesubmit}>
             <input
               type="text"
@@ -89,11 +91,16 @@ export default function Signup() {
               {error}
             </p>
           )}
-        
-          <p className="text-white p-1 text-center ">If you Have an Account   <Link to="/login"><span className="text-red-800 font-semibold uppercase p-2">  Login </span>
-            </Link></p>
 
-         
+          <p className="text-white p-1 text-center ">
+            If you Have an Account{" "}
+            <Link to="/login">
+              <span className="text-red-800 font-semibold uppercase p-2">
+                {" "}
+                Login{" "}
+              </span>
+            </Link>
+          </p>
         </div>
       </div>
     </div>
