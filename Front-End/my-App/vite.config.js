ac90b4fa26273921c875bdbd9f3f4d07dfc603d3
vijayakumar1069 <vijay.r20799@gmail.com>
2024-01-24 00:26:36 +0530

    try commits

--- Front-End/my-App/vite.config.js
+++ Front-End/my-App/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://inventory-app-01.onrender.com",
         secure: false,
       },
     },
