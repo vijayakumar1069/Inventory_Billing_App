352ae4077876243b9b3350d1f1442a0291943e01
vijayakumar1069 <vijay.r20799@gmail.com>
2024-02-06 01:17:54 +0530

    trail-06

--- Front-End/my-App/vite.config.js
+++ Front-End/my-App/vite.config.js
@@ -1,16 +1,16 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://inventory-app-01.onrender.com',
-//         secure: false,
-//       },
-//     },
-//   },
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://inventory-app-01.onrender.com',
+        secure: false,
+      },
+    },
+  },
 
-//   plugins: [react()],
-// });
+  plugins: [react()],
+});
